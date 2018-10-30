2ff05f4741f33728d592e10994ccf9542b73a702
Matej <matej.testen@gmail.com>
2018-10-29 18:33:45 -0700

    Added the first Rails project

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,21 @@
-puts 'Hello world I love you and I hope you dont hurt me'
+number = rand(100)
+guess = nil
+
+puts 'Welcome to the guessing game!'
+
+while guess != number do
+  print 'Enter your guess: '
+  guess = gets.to_i
+
+  print "Your guess of #{guess} "
+
+  if guess > number
+    print 'is too high'
+  elsif guess < number
+    print 'is too low'
+  else
+    print 'is exactly right! Congratulations!'
+  end
+
+  print "\n"
+end
